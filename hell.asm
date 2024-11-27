9140094fa2e7a6a2321fc7e94058dd2d014a7d4d
JuannCarloss <juancarlosbatistazeferino@gmail.com>
2024-11-26 22:54:46 -0300

    feat: first hello, world with assembly

---
+++ hell.asm
@@ -0,0 +1,22 @@
+global _start
+
+%define stdout 1
+%define syswrite 1
+%define exit_success 0
+%define sysexit 60
+
+section .data
+msg: db "hello, world", 0xA
+len: equ $ - msg
+
+section .text
+
+	mov rdi, stdout
+	mov rsi, msg
+	mov rdx, len
+	mov rax, syswrite
+	syscall
+
+	mov rdi, exit_success
+	mov rax, sysexit
+	syscall
